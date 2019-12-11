3e1912365b6f314314818d8e9b3ab5272ba5f66a
hiep <hiepg198@gmail.com>
2019-12-11 23:54:03 +0700

    bai tap oRm 1

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+
+class UserController extends Controller
+{
+    public function searchUser(Request $request)
+    {
+    	return view('client.pages.search_user');
+    }
+
+    public function resultSearch(Request $request)
+    {
+    	$idUser = $request->idUser;
+    	$name = $request->name;
+    	$class = $request->class;
+
+    	$result = User::where('id', 'like', "%$idUser%")
+    			    ->where('name', 'like', "%$name%")
+    			    ->where('class', 'like', "%$class%")
+    			    ->get();
+
+    	return view('client.pages.search_user', compact('result'));
+    }
+}
